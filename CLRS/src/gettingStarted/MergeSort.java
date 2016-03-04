a6aa3a18be15694485c9f1a45d3925bf4d655c33
Eliel Zamora <elielzamora@gmail.com>
2016-03-04 15:09:09 -0500

    Completed MergeSort which is now functioning correctly

--- CLRS/src/gettingStarted/MergeSort.java
+++ CLRS/src/gettingStarted/MergeSort.java
@@ -1,6 +1,10 @@
 package gettingStarted;
 
 public class MergeSort {
+	/**
+	 * array must not contain the sentinel value
+	 */
+	private static int SENTINEL = Integer.MAX_VALUE; 
 	private static int[] array = {5,2,4,6,1,3};
 	public static void main(String ... args){
 		System.out.println("before sorting:");
@@ -11,33 +15,48 @@ public class MergeSort {
 	}
 	/**
 	 * divide and conquer
-	 * divide: sort both halves of the array recursivley
+	 * divide: split array into two sort both halves using mergeSort
 	 * conquer: aha! an element of size 1 is sorted
 	 * combine: user merge method
 	 * @param A
 	 */
 	public static void mergeSort(int[] A){
-		if (A.length < 2) return; //Already sorted
-		int j;
-		for(int i = 1; i < A.length; i ++){
-			j = i;
-			while (j > 0 && A[j-1]> A[j]){
-				swap(A, j, j-1);
-				j--;
-			}
+		mergeSort(A, 0, A.length-1);
+	}
+	public static void mergeSort(int[] A, int p, int r){
+		if(p < r) {
+			int q = (int) Math.floor((p+r)/2);
+			mergeSort(A, p, q);
+			mergeSort(A, q+1, r);
+			merge(A, p, q, r);
 		}
 	}
-	public static void swap(int[] A, int i, int j){
-		assert i < A.length && j < A.length; 
-		int temp = A[i];
-		A[i] = A[j];
-		A[j] = temp;
-	}
+	/** */
 	public static void merge(int[] A, int p, int q, int r){
-		assert i < A.length && j < A.length; 
-		int temp = A[i];
-		A[i] = A[j];
-		A[j] = temp;
+		//assert p < q && q <= r;
+		System.out.println("p: "+ p + " q: " + q + " r: " + r);
+		int l1 = (q-p) + 1;
+		int l2 = (r-q);
+		int[] a1 = new int[l1+1];
+		int[] a2 = new int[l2+1];
+		for(int i = 0; i < l1; i++) a1[i] = A[i+p];
+		for(int i = 0; i < l2; i++) a2[i] = A[i+q+1];
+		a1[l1] = SENTINEL;
+		a2[l2] = SENTINEL;
+		System.out.println("merging:");
+		printArray(a1);
+		System.out.println("with:");
+		printArray(a2);
+		int i = 0, j = 0;
+		for(int k = p; k <= r; k++){
+			if(a1[i] <= a2[j]){
+				A[k] = a1[i];
+				i++;
+			}else{
+				A[k] = a2[j];
+				j++;
+			}
+		}
 	}
 	public static void printArray(int[] A){
 		for(int i: A){
