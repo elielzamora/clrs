a149ea26abdf68f4b242a72a11114b7c7dac10df
Eliel Zamora <elielzamora@gmail.com>
2016-03-05 00:21:33 -0500

    Creates Max Sub array class for chapter 4 on divide and conquer
    Brute force method is working

--- CLRS/src/divideAndConquer/MaximumSubArrayProblem.java
+++ CLRS/src/divideAndConquer/MaximumSubArrayProblem.java
@@ -1,6 +1,7 @@
 package divideAndConquer;
 
 public class MaximumSubArrayProblem {
+	public static final int SENTINEL = Integer.MIN_VALUE;
 	/** array represents change in prices
 	 *  and the maximum sub-array of the change in prices
 	 *  represents the greatest profit achievable in
@@ -9,9 +10,39 @@ public class MaximumSubArrayProblem {
 	public static final int[] array = 
 		{13, -3, -25, 20, -3, -16, -23, 18,
 		 20, -7, 12, -5, -22, 15, -4, 7};
+	
 	public static void main(String[] args) {
+		System.out.println("Max Sub Array");
+		printArray(array);
+		int[] subdata = bruteForce(array);
+		System.out.println("l: " + subdata[0]);
+		System.out.println("r: " + subdata[1]);
+		System.out.println("max: " + subdata[2]);
+		printSubArray(array, subdata[0], subdata[1]);
 		
 	}
-	//public static final int[] 
-
+	public static int[] bruteForce(int[] A){
+		int n = A.length, sum = 0, l = 0, r = 0,
+				max = SENTINEL;
+		for(int i = 0; i < n; i++){
+			for(int j = i; j < n; j++){
+				sum += A[j];
+				if(sum >= max){
+					l = i;
+					r = j;
+					max = sum;
+				}
+			}
+			sum = 0;
+		}
+		return new int[]{l, r, max};
+	}
+	public static void printArray(int[] array){
+		for(int i : array){
+			System.out.println(i);
+		}
+	}
+	public static void printSubArray(int[] array, int a, int b){
+		for(int i = a; i <= b; i++) System.out.println(array[i]);
+	}
 }
