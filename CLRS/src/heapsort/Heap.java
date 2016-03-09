ed01b1dd0ad2659972bfb206d3e890374c510c81
Eliel Zamora <elielzamora@gmail.com>
2016-03-08 22:44:42 -0500

    Heap and Heapsort classes almost complete
    Still contain errors

--- CLRS/src/heapsort/Heap.java
+++ CLRS/src/heapsort/Heap.java
@@ -23,7 +23,9 @@ public class Heap {
 	public int get(int i){return this.heap[i];}
 	public int size(){return heap.length;}
 	public void swap(int i, int j){
-		
+		int temp = this.get(i);
+		this.heap[i] = j;
+		this.heap[j] = temp;
 	}
 	public void heapify(int i){
 		int largest = i;
@@ -38,9 +40,39 @@ public class Heap {
 		}
 	}
 	public void buildHeap(){
+		for(int i = this.size()/ 2; i > -1; i++){
+			this.heapify(i);
+		}
+	}
+	public int max() throws heapUnderflowException{
+		if(this.size() > 0)
+			return this.get(0);
+		else throw new heapUnderflowException();
+	}
+	public int extractMax(){
+		return 0;
+	}
+	/**
+	 * apply heapsort to this heap
+	 */
+	public void sort(){
 		
 	}
-	public void heapSort(){
-		
+	public static void main(String ... args){
+		Heap heap = new Heap(new int[]{8,3, 9, 2, 3});
+		heap.buildHeap();// redundant
+		for(int i = 0; i < heap.size(); i++){
+			System.out.println(heap.get(i));
+		}
+		//int[] sorted = heap.sort();
+		//for(int i : sorted) System.out.println(i);
 	}
+}
+class heapUnderflowException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 }
--- CLRS/src/heapsort/Heapsort.java
+++ CLRS/src/heapsort/Heapsort.java
@@ -1,5 +1,9 @@
 package heapsort;
 
 public class Heapsort {
-	
+	public static int[] array = {3, 4, 7, 1, 2, 5, 8};
+	public static void main(String ... args){
+		Heap heap = new Heap (array);
+		heap.sort();
+	}
 }
