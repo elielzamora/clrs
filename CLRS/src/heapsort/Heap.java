8c16b7cfdc463a48d92ab2cf68157fd8c79d7635
Eliel Zamora <elielzamora@gmail.com>
2016-03-09 23:57:31 -0500

    Heap and Heapsort work
    Are now funcitonal
    
    Note: error was caused by heapify taking left element even if right was 
    greater since it only compared to the original parent this mistake is
    now fixed and work as designed.

--- CLRS/src/heapsort/Heap.java
+++ CLRS/src/heapsort/Heap.java
@@ -10,7 +10,7 @@ public class Heap {
 	private int size;
 	public Heap(int[] h){
 		this.heap = h;
-		this.buildHeap();
+		this.build();
 	}
 	public Heap(int s){
 		this(new int[s]);
@@ -18,12 +18,12 @@ public class Heap {
 	public int parent (int i){return (int)((i-1)/2);}
 	public int left (int i){return i * 2 + 1;}
 	public int right(int i){return (i+1) * 2;}
-	public int max() throws heapUnderflowException {
+	public int max() throws HeapUnderflowException {
 		if (heap.length != 0) return 0;
-		else throw new heapUnderflowException();}
-	public int getParent(int i){return this.heap[parent(i)];}
-	public int getLeft(int i){return this.heap[left(i)];}
-	public int getRight(int i){return this.heap[right(i)];}
+		else throw new HeapUnderflowException();}
+	public int getParent(int i){return this.get(parent(i));}
+	public int getLeft(int i){return this.get(left(i));}
+	public int getRight(int i){return this.get(right(i));}
 	public int get(int i){return this.heap[i];}
 	public int size(){return this.size;}
 	public int length(){return this.heap.length;}
@@ -32,11 +32,23 @@ public class Heap {
 		this.heap[i] = this.get(j);
 		this.heap[j] = temp;
 	}
+	public void check()throws NotMaxHeapException{
+		for(int i = 0; i < this.size(); i++){
+			boolean heapProp = true;
+			if(left(i) < size() && get(i) < getLeft(i))
+				heapProp = false;
+			if(right(i) < size() && get(i) < getRight(i))
+				heapProp = false;
+			if(heapProp) continue;
+			throw new NotMaxHeapException(i);
+		}
+	}
 	public void heapify(int i){
 		int largest = i;
 		if(left(i) < size() && get(i) < getLeft(i)){
 			largest = left(i);
-		}else if(right(i) < size() && get(i) < getRight(i)){
+		}
+		if(right(i) < size() && get(largest) < getRight(i)){
 			largest = right(i);
 		}
 		if(largest != i){
@@ -44,56 +56,62 @@ public class Heap {
 			heapify(largest);
 		}
 	}
-	public void buildHeap(){
+	public void build(){
 		this.size = this.heap.length;
-		for(int i = this.size()/ 2 + 1; i > -1; i--){
+		for(int i = (this.size()-1)/ 2; i > -1; i--){
 			this.heapify(i);
 		}
 	}
-	public int getMax() throws heapUnderflowException{
+	public int getMax() throws HeapUnderflowException{
 		return this.get(max());
 	}
-	public int extractMax(){
+	/*public int extractMax(){
 		return 0;
-	}
+	}*/
 	/**
 	 * apply heapsort to this heap
 	 * the result is that it is no longer a heap
 	 * but it is a sorted array.
+	 * @throws NotMaxHeapException 
 	 * @throws heapUnderflowException 
 	 */
-	public void sort() throws heapUnderflowException{
-		while(size() >0){
-			System.out.println("max: " + get(max()));
+	public void sort() throws HeapUnderflowException, NotMaxHeapException{
+		this.build();
+		while(size() > 0){
 			swap(max(), size() - 1); // swap largest with last element
 			this.size--;
 			heapify(max());
+			this.check();//for debugging
 		}
 	}
-	public static int[] count(int s, int t){
-		assert s < t;
-		int[] count = new int[t-s];
-		for(int i = s; i < t; i++){
-			count[i] = i;
+	public static int[] randomArray(int size, int max){
+		int[] rnd = new int[size];
+		for(int i = 0; i < size; i ++){
+			rnd[i] = (int)(max * Math.random());
 		}
-		return count;
+		return rnd;
 	}
-	public static void main(String ... args){
-		Heap heap = new Heap(new int[]{1, 8, 3, 9, 2, 5, 7});
-		heap.buildHeap();// redundant
+	public static void main(String ... args) throws NotMaxHeapException{
+		//Heap heap = new Heap(new int[]{1, 8, 3, 9, 2, 5, 7});
+		//Heap heap = new Heap(randomArray(10, 100));
+		Heap heap = new Heap(new int[]{5,13,2,25,7,17,20,8,4});
+		//heap.check();
 		for(int i = 0; i < heap.size(); i++){
-			System.out.println(heap.get(i));
-		}
+			System.out.println(heap.get(i));}
 		try {
 			heap.sort();
-		} catch (heapUnderflowException e) {
+			System.out.println("printing sorted array");
+			for(int i = 0; i < heap.length(); i++){
+				System.out.println(heap.get(i));
+			}
+		} catch (HeapUnderflowException e) {
 			System.out.println("whoops!");
-		}
-		System.out.println("printing sorted array");
-		for(int i = 0; i < heap.length(); i++){
-			System.out.println(heap.get(i));
-		}
-		
+		} catch (NotMaxHeapException e){
+			System.out.println("was good until: " + e.getMessage());
+			for(int i = 0; i < heap.length(); i++){
+				System.out.println(heap.get(i));
+			}
+		}	
 		/*System.out.println("debug");
 		for(int i : count(0,5)){
 			System.out.println("i:     " + i);
@@ -107,4 +125,16 @@ public class Heap {
 }
 //** exceptions
 @SuppressWarnings("serial")
-class heapUnderflowException extends Exception{}
+class HeapUnderflowException extends Exception{}
+@SuppressWarnings("serial")
+class NotMaxHeapException extends Exception{
+	int end;
+	public NotMaxHeapException(){}
+	public NotMaxHeapException(int end) {
+		this.end = end;
+	}
+	@Override
+	public String getMessage() {
+		return String.valueOf(this.end);
+	}
+}
--- CLRS/src/heapsort/Heapsort.java
+++ CLRS/src/heapsort/Heapsort.java
@@ -1,10 +1,13 @@
 package heapsort;
 
 public class Heapsort {
-	public static int[] array = {3, 4, 7, 1, 2, 5, 8};
+	public static int[] array = {3, 6, 4, 7, 1, 2, 5, 8};
 	public static void main(String ... args)
-			throws heapUnderflowException{
+			throws HeapUnderflowException, NotMaxHeapException{
 		Heap heap = new Heap (array);
 		heap.sort();
+		for(int i = 0; i < heap.length(); i++){
+			System.out.println(heap.get(i));
+		}
 	}
 }
